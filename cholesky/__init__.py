9f3d8ec0ad851830459c451f82de88dd2b621457
Kyle <bkeskridge@email.wm.edu>
2020-06-26 15:16:19 -0400

    Fixed incorect relative import

--- cholesky/__init__.py
+++ cholesky/__init__.py
@@ -2,6 +2,6 @@ import sys
 
 #TODO: fix the import below - should only imporant the main Cholesky object
 if sys.version_info[0] >= 3:
-    from Cholesky_utils_py3 import *
+    from .Cholesky_utils_py3 import *
 else:
     raise Exception("Not Implemented for Python 2 : Must use Python 3")
