46dfc8fced5cf0695459a33d6bc64532f6200998
Kyle Eskridge <bkeskr@bora.sciclone.wm.edu>
2020-08-12 14:24:52 -0400

    fixed a typo

--- assign/__init__.py
+++ assign/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-#TODO: fix the import below - should only imporant the main Cholesky object
+#TODO: fix the import below - should only import the main Cholesky object
 if sys.version_info[0] >= 3:
     from .orbital_assign_utils_py3 import *
 elif sys.version_info[0] < 3:
