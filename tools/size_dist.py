4d7a81e09b145896251b1cccab7a492fcc823362
Kyle Eskridge <bkeskr@bora.sciclone.wm.edu>
2020-08-12 14:27:01 -0400

    Promoted _tools to submodule

--- tools/size_dist.py
+++ tools/size_dist.py
@@ -12,7 +12,11 @@ def compute_sigma2(mol, C):
     - we are computing centroids that agree with ERKALE, but mu_2 / sigma_2 do not agree
        - this confirms that we have the correct convention for C as well
        - also confirms that we are working in Bohr!
-
+    - IMPORTANT! C must be a np.matrix and not a np.array in the current implementation! 
+                 This has to do with the arithmetic operator's defition of * which is different
+                 for np.array, and np.matrix
+      - can use einsum, or np.matul to allow arrays too
+      - if that fails, add a type check
     '''
     M,N = C.shape
     
@@ -20,7 +24,10 @@ def compute_sigma2(mol, C):
 
     rsq = mol.intor('int1e_r2')
     r = mol.intor('int1e_r')
-    
+
+    sigma_sum=0.0
+    mu_sum=0.0
+
     print(f'\n\n{"i":>5} {"mu_2":>12} {"sigma_2":>12} {"Centroid (x,y,z)":>26}')
     for i in range(N):
         # compute <r>
@@ -33,7 +40,11 @@ def compute_sigma2(mol, C):
         print(f'expect_rsq = {expect_rsq} with shape {expect_rsq.shape}')
         centroid_sq = np.dot(centroid,centroid)'''
         mu_2 = (C_dag[i,:]*rsq*C[:,i] - np.dot(centroid,centroid))[0,0]
-        #mu_2 = expect_rsq + 
+        
         sigma_2 = np.sqrt(mu_2)
         print(f'{i:>5} {mu_2:>12.8e} {sigma_2:>12.8e} ({centroid[0]:>8.6e},{centroid[1]:>8.6e},{centroid[2]:>8.6e})')
+        sigma_sum+=sigma_2
+        mu_sum+=mu_2
         
+    print(f'sum of sigma_2 over all orbitals is : {sigma_sum}')
+    print(f' [FB cost function] sum of mu_2 over all orbitals is : {mu_sum}')
