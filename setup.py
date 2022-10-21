9c5b1bb14bb16480889edae259478c902dc7e9d0
Kyle <8075511+bkesk@users.noreply.github.com>
2022-10-21 12:56:20 -0400

    tweak package definition

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='embedding4afqmc',
+    name='embedding',
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'numpy', 'h5py', 'scipy', 'pyscf'
+        'numpy', 'h5py', 'scipy', 'pyscf', 'pyyaml'
     ],
 )
